f0e0cd4e8b080bde151214b08659de966e4d6574
Larry Weya <larryweya@gmail.com>
2013-06-27 10:30:26 +0300

    Added angular, bamboo.js and nv.d3 charts

---
+++ github-africa/angular-app/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('GithubAfrica',
+    ['GithubAfrica.controllers', 'GithubAfrica.directives', 'GithubAfrica.filters', 'GithubAfrica.services']);
