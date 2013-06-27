3b3d50c9366b70ab907a57b009a11541647b8090
Larry Weya <larryweya@gmail.com>
2013-06-27 10:19:25 +0300

    Revert "added angular and nv.d3 charts"
    This reverts commit 7db09c3d41f8f38516cb5b6c69a90375b0dc10a3.

--- github-africa/angular-app/filters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-angular.module('GithubAfrica.filters', []).
-    filter('interpolate', ['version', function(version) {
-    return function(text) {
-        return String(text).replace(/\%VERSION\%/mg, version);
-    }
-}]);
